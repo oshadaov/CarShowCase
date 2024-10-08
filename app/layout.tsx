4bfd08ab71112f6861e86bc56761036fc9ca7bd2
oshadaov <oshadaov@gmail.com>
2024-10-08 23:07:14 +0530

    created footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,18 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { Navbar,Footer } from "@/components";
 import "./globals.css";
-import { Footer,Navbar } from "@/components";
 
-
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export const metadata: Metadata = {
   title: "Car Hub",
