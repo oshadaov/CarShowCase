13a6cba8ac4bf693e4a286379fcca2c8d7cd0e0f
oshadaov <oshadaov@gmail.com>
2024-10-13 11:20:49 +0530

    created CardCar component

--- components/index.ts
+++ components/index.ts
@@ -5,6 +5,8 @@ import Footer from './Footer'
 import SearchBar from './SearchBar'
 import CustomFilter from './CustomFilter'
 import SearchManufacturer from './SearchManufacturer'
+import CarCard from './CarCard'
+
 export{
     Hero,
     CustomButton,
@@ -12,8 +14,8 @@ export{
     Footer,
     SearchBar,
     CustomFilter,
-    SearchManufacturer
-
+    SearchManufacturer,
+    CarCard
 
 
 
